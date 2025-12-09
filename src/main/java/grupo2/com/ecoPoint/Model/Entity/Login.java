e1c1b7a39d2262946b0653c6ad4f7c425dcf15e2
ThaisMiwa <thaiscp59@gmail.com>
2025-12-09 17:04:21 -0300

    teste

---
+++ src/main/java/grupo2/com/ecoPoint/Model/Entity/Login.java
@@ -0,0 +1,39 @@
+package grupo2.com.ecoPoint.Model.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String usuario;
+    private String senha;
+
+    public Login() {}
+
+    public Login(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+        public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
